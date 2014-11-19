40fa2c40715b756c5c245feaa7841a15eaee805e
thinq4yourself <joel@thinq4yourself.com>
2014-11-19 06:40:19 +0000

    updated some more UI (panels, animations, box shadows, etc) for speed & UX

--- public/res/extensions/mysyncURL.js
+++ public/res/extensions/mysyncURL.js
@@ -1,12 +1,27 @@
 define([
 	"jquery",
 	"underscore",
+    "utils",
+    "settings",
 	'classes/Extension',
     "text!html/mysyncURLSettingsBlock.html"
-], function(_, $, Extension, mysyncURLSettingsBlockHTML) {
+], function(_, $, utils, settings, Extension, mysyncURLSettingsBlockHTML) {
 
 	var mysyncURL = new Extension('mysyncURL', 'My.WriteOn Sync URL', false, true);
     mysyncURL.settingsBlock = mysyncURLSettingsBlockHTML;
-	return mysyncURL;
+    mysyncURL.defaultConfig = {
+        couchdburl: settings.couchdbUrl
+    };
+
+    mysyncURL.onLoadSettings = function() {
+        utils.setInputValue("#input-settings-couchdb-url", mysyncURL.config.couchdburl);
+    };
+
+    mysyncURL.onSaveSettings = function(newConfig, event) {
+        newConfig.couchdburl = utils.getInputIntValue("#input-settings-couchdb-url", event, 1, 60000);
+    };
+    
+    
+    return mysyncURL;
 
 });
