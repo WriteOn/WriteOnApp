55925e805ad67926ca10ae3c2d2cc9401b1a0fb5
thinq4yourself <joel@thinq4yourself.com>
2015-06-15 18:49:41 +0000

    updated GTM for deploy, updated https redirect in server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var app = require('./writeon.server');
 /* 
  * FORCE HTTPS
  */
-/* Used to force SSL - required for security */
+/* Used to force SSL - required for security 
 app.all('*', function(req, res, next) {
     if(req.headers['x-forwarded-proto'] != 'https') {
 		return res.redirect('https://' + req.headers.host + req.path);
@@ -38,18 +38,23 @@ app.all('*', function(req, res, next) {
 	/\.(eot|ttf|woff|svg|png)$/.test(req.url) && res.header('Access-Control-Allow-Origin', '*');
 	next();
 });
-/* Legacy ...
+*/
+
+/* Legacy support ... per domain */
 app.all('*', function(req, res, next) {
 	if (req.headers.host == 'writeon.io' && req.headers['x-forwarded-proto'] != 'https') {
 		return res.redirect('https://writeon.io' + req.url);
 	}
 	if (req.headers.host == 'beta.writeon.io' && req.headers['x-forwarded-proto'] != 'https') {
-		return res.redirect('https://beta.writeon.io' + req.url);
+		return res.redirect('https://writeon.io' + req.url);
+	}
+	if (req.headers.host == 'next.writeon.io' && req.headers['x-forwarded-proto'] != 'https') {
+		return res.redirect('https://next.writeon.io' + req.url);
 	}
 	/\.(eot|ttf|woff|svg)$/.test(req.url) && res.header('Access-Control-Allow-Origin', '*');
 	next();
 });
-*/
+
 /* 
  * NODE CLUSTERING
  */
