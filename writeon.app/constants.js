9aba29804aa15de21988a90d4b8b2aa093340c78
thinq4yourself <joel@thinq4yourself.com>
2015-06-17 16:42:27 +0000

    Prepared & released: v1.9.25.

--- writeon.app/constants.js
+++ writeon.app/constants.js
@@ -5,7 +5,7 @@ define([], function() {
 	var constants = {};
 	
 	// App dependent
-	constants.VERSION = "1.9.24";
+	constants.VERSION = "1.9.25";
 	constants.MAIN_URL = "https://writeon.io/";
 	constants.DEFAULT_FILE_TITLE = "Story Title";
 	constants.DEFAULT_FOLDER_NAME = "New Storybook";
